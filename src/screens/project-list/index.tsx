c92ddd1894b0e4853d31e232650396ce694fc0b0
qiwang44 <qiwang44@iflytek.com>
2021-07-02 17:22:45 +0800

    初始化

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import * as qs from "qs";
+import { useMount, useDebounce, cleanObject } from "util/index";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // 获取list接口
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
---
+++ src/util/index.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
+
+export const cleanObject = (object?: { [key: string]: unknown }) => {
+  if (!object) {
+    return {};
+  }
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isVoid(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callBack: () => void) => {
+  useEffect(() => {
+    callBack();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export const useDebounce = <V>(value: V, delay?: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
+};
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
+};
